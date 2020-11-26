12568c0a3d7a1ebe83edcec6f833993e1fee4ed6
biancasobral <biasobral95@gmail.com>
2020-11-26 19:12:26 -0300

    fix commit

---
+++ Aula10_SQLiteWebApp/src/java/db/TasksConnector.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.util.ArrayList;
+import java.sql.*;
+
+public class TasksConnector {
+    private static final String CLASS_NAME = "org.sqlite.JDBC";
+    private static final String URL = "jdbc:sqlite:/Users/biancasobral/Desktop/tasks-noturno.db";
+    
+    public static ArrayList<String> select() throws Exception {
+        ArrayList<String> list = new ArrayList<>();
+        Class.forName(CLASS_NAME);
+        Connection con = DriverManager.getConnection(URL);
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM Tasks");
+        while(rs.next()){
+            list.add(rs.getString("name"));
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+       return list;
+    }
+    public static void insert(String name) throws Exception {
+        Class.forName(CLASS_NAME);
+        Connection con = DriverManager.getConnection(URL);
+        Statement stmt = con.createStatement();
+        stmt.execute("insert into Tasks values('"+ name + "')");
+        stmt.close();
+        con.close();
+      
+    }
+    public static void delete(String name) throws Exception {
+        Class.forName(CLASS_NAME);
+        Connection con = DriverManager.getConnection(URL);
+        Statement stmt = con.createStatement();
+        stmt.execute("delete from Tasks where name ='"+ name + "'");
+        stmt.close();
+        con.close();
+      
+    }
+    
+    //Criar métodos: select(); insert(); delete().
+    //SENDO QUE O SELECT RETORNA UM ARRAYLIST DE STRINGS.
+    
+}
